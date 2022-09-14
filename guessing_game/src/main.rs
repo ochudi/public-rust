c0aebd22ebe3166d854305ab081e752d33dd7ca8
ochudi <chukwudi.ofoma@pau.edu.ng>
2022-09-14 10:13:10 +0100

    Modified guessing game and began minigrep project

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -18,7 +18,10 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("{}", "Invalid input!\nAn integer is required.".red());
+                continue
+            },
         };
 
         println!("You guessed: {guess}");
